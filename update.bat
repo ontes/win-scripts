03c68aecf16da3655b0d70564370f27e487b5a50
OnTes <ontes@post.cz>
2021-03-18 10:04:42 +0100

    Update for latest Windows

--- update.bat
+++ update.bat
@@ -1,11 +1,7 @@
 @echo off
- 
 set wd=C:\update
 
 md %wd%
-expand "%~dp0\*.msu" %wd%
-
-for %%a in (%wd%\windows10.0-kb*-x64.cab) do dism /online /add-package /packagepath=%%a /norestart
-for %%a in (%wd%\windows10.0-kb*-x64_psfx.cab) do dism /online /add-package /packagepath=%%a /norestart
-
+expand -f:* "%~dp0\*.msu" %wd%
+for %%a in (%wd%\windows*.cab) do dism /online /add-package /packagepath=%%a
 rd %wd% /s /q
